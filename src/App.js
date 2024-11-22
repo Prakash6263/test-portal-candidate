fc17b9194dae77e0d6643f89ae666ad493cbbe8b
Anurag <johncarlosanu@gmail.com>
2024-11-22 11:53:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/home'
+import SignUp from './pages/Signup';
+import Success from './pages/success';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/information" element={<SignUp />} />
+            <Route path="/success" element={<Success />} />
+           
+           
+          </Routes>
+      </Router>
     </div>
   );
 }
